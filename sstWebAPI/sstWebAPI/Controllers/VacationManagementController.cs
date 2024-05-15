f05685dfc98d14ba85c6357bab49f3ca4fba04f3
Biellbo <lukas.trapp@outlook.de>
2024-05-15 21:02:10 +0200

    created endpoints to get vacation applications

---
+++ sstWebAPI/sstWebAPI/Controllers/VacationManagementController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SelfServiceWebAPI.Models;
+using SelfServiceWebAPI;
+using sstWebAPI.Constants;
+using sstWebAPI.Helpers;
+using sstWebAPI.Models.DB;
+using sstWebAPI.Models.DTO.Vacation;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace sstWebAPI.Controllers
+{
+    /// <summary>
+    /// controller for creating and editing vacation application
+    /// </summary>
+    /// <remarks>
+    /// constructor for the vacation controller
+    /// </remarks>
+    /// <param name="context"></param>
+    /// <param name="config"></param>
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class VacationManagementController(AppDbContext context) : Controller
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        [Authorize(Roles = UserRoles.Admin +","+UserRoles.Management)]
+        public IActionResult GetVacationApplicationsOfUser(Guid user_id)
+        {
+            var applications = user_id == Guid.Empty ? _context.vacation_request.ToList() : _context.vacation_request.Where(x => x.ID_user == user_id).ToList();
+
+            var pending = new List<VacationApplicationModel>();
+            var accepted = new List<VacationApplicationModel>();
+            var declined = new List<VacationApplicationModel>();
+
+            foreach (var application in applications)
+            {
+                if (application.state == VacationApplicationStates.Pending)
+                {
+                    pending.Add(application);
+                }
+                else if (application.state == VacationApplicationStates.Accepted)
+                {
+                    accepted.Add(application);
+                }
+                else
+                {
+                    declined.Add(application);
+                }
+            }
+
+            return Ok(new GetVacationApplicationsModel(pending, accepted, declined));
+        }
+
+
+        #region helper_functions
+
+        #endregion
+
+    }
+}
---
+++ sstWebAPI/sstWebAPI/Models/DTO/Vacation/GetVacationApplicationsModel.cs
@@ -0,0 +1,37 @@
+﻿using sstWebAPI.Models.DB;
+
+namespace sstWebAPI.Models.DTO.Vacation
+{
+    public class GetVacationApplicationsModel
+    {
+        #region constructors
+
+        public GetVacationApplicationsModel(List<VacationApplicationModel> pending, List<VacationApplicationModel> accepted, List<VacationApplicationModel> declined) 
+        {
+            pending_applications = pending;
+            accepted_applications = accepted;
+            declined_applications = declined;
+        }
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// List of pending applications
+        /// </summary>
+        public List<VacationApplicationModel> pending_applications { get; set; }
+
+        /// <summary>
+        /// List of accepted applications
+        /// </summary>
+        public List<VacationApplicationModel> accepted_applications { get; set; }
+
+        /// <summary>
+        /// List of declined applications
+        /// </summary>
+        public List<VacationApplicationModel> declined_applications { get; set; }
+
+        #endregion
+    }
+}
