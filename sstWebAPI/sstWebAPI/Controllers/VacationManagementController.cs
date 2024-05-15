50f9ba6dff9924029d8872ecb924ad6d9e0380ba
Biellbo <lukas.trapp@outlook.de>
2024-05-15 21:40:58 +0200

    implented endpoint for processing vacation application

--- sstWebAPI/sstWebAPI/Controllers/VacationManagementController.cs
+++ sstWebAPI/sstWebAPI/Controllers/VacationManagementController.cs
@@ -54,6 +54,65 @@ namespace sstWebAPI.Controllers
             return Ok(new GetVacationApplicationsModel(pending, accepted, declined));
         }
 
+        [HttpPatch]
+        [Authorize(Roles = UserRoles.Admin + "," + UserRoles.Management)]
+        public IActionResult EditVacationApplication(EditVacationApplicationModel model)
+        {
+            var application = _context.vacation_request.Find(model.application_id);
+            if (application == null)
+            {
+                return NotFound(model.application_id);
+            }
+
+            var vacationDays = _context.vacation_days.Where(x => x.ID_user == application.ID_user).FirstOrDefault();
+            if (vacationDays == null)
+            {
+                return NotFound("No vacation days found");
+            }
+
+            if (model.state == VacationApplicationStates.Pending)
+            {
+                if(application.state == VacationApplicationStates.Accepted)
+                {
+                    vacationDays.used_days -= application.number_of_days;
+                    vacationDays.planned_days += application.number_of_days;
+                }else if(application.state == VacationApplicationStates.Declined)
+                {
+                    vacationDays.planned_days += application.number_of_days;
+                }
+                application.state = VacationApplicationStates.Pending;
+            } else if (model.state == VacationApplicationStates.Accepted)
+            {
+                if (application.state == VacationApplicationStates.Pending)
+                {
+                    vacationDays.planned_days -= application.number_of_days;
+                    vacationDays.used_days += application.number_of_days;
+                }
+                else if (application.state == VacationApplicationStates.Declined)
+                {
+                    vacationDays.used_days += application.number_of_days;
+                }
+                application.state = VacationApplicationStates.Accepted;
+            } else if (model.state == VacationApplicationStates.Declined)
+            {
+                if (application.state == VacationApplicationStates.Pending)
+                {
+                    vacationDays.planned_days -= application.number_of_days;
+                }
+                else if (application.state == VacationApplicationStates.Accepted)
+                {
+                    vacationDays.used_days -= application.number_of_days;
+                }
+                application.state = VacationApplicationStates.Declined;
+            } else
+            {
+                return BadRequest($"unknown state {model.state}");
+            }
+            application.reason = model.reason;
+            _context.SaveChanges();
+            return Ok();
+        }
+
 
         #region helper_functions
 
---
+++ sstWebAPI/sstWebAPI/Models/DTO/Vacation/EditVacationApplicationModel.cs
@@ -0,0 +1,24 @@
+﻿namespace sstWebAPI.Models.DTO.Vacation
+{
+    public class EditVacationApplicationModel
+    {
+        #region properties
+
+        /// <summary>
+        /// ID of application which is being processed
+        /// </summary>
+        public Guid application_id { get; set; }
+
+        /// <summary>
+        /// new state of application
+        /// </summary>
+        public string state { get; set; }
+
+        /// <summary>
+        /// reason for this application being accepted/declined
+        /// </summary>
+        public string reason { get; set; }
+
+        #endregion
+    }
+}
